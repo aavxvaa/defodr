831ec416dc194720229aa5a1184cd87b90cc4893
Liu Jian <871007566@qq.com>
2018-03-18 23:33:38 +0800

    Create TT.py

---
+++ wanwanwan/TT.py
@@ -0,0 +1,35 @@
+def $n=$b.p
+    $l=$w-$n
+    print $l=$b.r
+def d.r=d.k 
+    d.ft.p=（1，2.3.4......）
+    print R
+	
+for 1r.dr.l==0
+    1er.dr.a==3
+    then p.kg=1er.dr.le
+print R
+print de.pn=$（1，2.3.4......)
+
+def de.pn=$(1/n)
+     n=ne.de.p
+	 print W
+	 return 0
+else 1er.dr.le>1
+     1er.dr.e<3
+
+for t.n=0.e.API	 
+else cy.eh=R
+     c>eT/H^2
+then  my.c.eh=$PE
+      f.c.eh=cy.e
+	  t.cy.eh><te.w
+
+FOR 1er.dr.le==0
+    1er.dr.ae==3 
+    THEN pn==D 
+IF	dae.pn=$(1/n)  
+   THEN RT=SP
+       
+PRINT
+	d.pn=(1，2.3.4......)
